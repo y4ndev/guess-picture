6d420e5c7f5113b826f3cb629edcfdc5447980d7
Yan Golovchenko <y4nweb@gmail.com>
2024-10-01 22:18:53 +0300

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { CategoryCard } from "@/components/CategoryCard/CategoryCard";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>Home page</div>
+      <div className={styles.inner}>
+        <h1>Guess-picture</h1>
+        <div className={styles.categorys}>
+          <CategoryCard />
+        </div>
+        <button className={styles.btn}>Start</button>
+      </div>
     </main>
   );
 }
--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,7 +1,37 @@
+"use client";
 import React from "react";
+import { useState } from "react";
+import styles from "./CategoryCard.module.scss";
 
-const CategoruCard = () => {
-  return <div>CategoruCard</div>;
+const CategoryCard = () => {
+  const [activeCategory, setActiveCategory] = useState<number | null>(null);
+
+  const catItems = [
+    { name: "Films", id: 1 },
+    { name: "Series", id: 2 },
+    { name: "Cartoons", id: 3 },
+    { name: "Games", id: 4 },
+  ];
+
+  const handleClick = (categoryId: number) => {
+    setActiveCategory(categoryId); // Обновляем состояние, указывая активный элемент
+  };
+
+  return (
+    <div className={styles.categorys}>
+      <ul className={styles.items}>
+        {catItems.map((category) => (
+          <li
+            className={`${styles.item} ${activeCategory === category.id ? styles.active : ""}`}
+            key={category.id}
+            onClick={() => handleClick(category.id)}
+          >
+            {category.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default { CategoruCard };
+export { CategoryCard };
