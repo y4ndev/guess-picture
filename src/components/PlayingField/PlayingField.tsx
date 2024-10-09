ca391db35e0d45cfaf2a8ec488b93df50d6092a1
Yan Golovchenko <y4nweb@gmail.com>
2024-10-09 22:08:12 +0300

    push

--- src/components/PlayingField/PlayingField.tsx
+++ src/components/PlayingField/PlayingField.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { films } from "@/utils/imageFilms";
+import Image from "next/image";
 
 const PlayingField = () => {
-  return <div>PlayingField</div>;
+  return (
+    <div>
+      {films.map((film) => (
+        <div key={film.id}>
+          <h3>{film.title}</h3>
+          <Image src={film.image} alt={film.title} width={300} height={450} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default { PlayingField };
