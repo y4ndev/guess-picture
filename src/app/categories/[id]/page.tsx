194972da26c33c3646bbd9d6f6188ce63b9f202d
Yan Golovchenko <y4nweb@gmail.com>
2024-11-01 23:17:14 +0300

    push

--- src/app/categories/[id]/page.tsx
+++ src/app/categories/[id]/page.tsx
@@ -5,17 +5,33 @@ import { cartoons } from "@/utils/imageCartoons";
 import { series } from "@/utils/imageSeries";
 import { films } from "@/utils/imageFilms";
 import Image, { StaticImageData } from "next/image";
-import { useState } from "react";
+import { use, useState } from "react";
 import { GamePictures } from "@/components/GamePictures/GamePictures";
 
 interface ICategoryImages extends ICategory {
   images: StaticImageData[];
+  filmId: number[];
 }
 
 const categories: ICategoryImages[] = [
-  { id: 1, name: "Films", images: films.map((film) => film.image) },
-  { id: 2, name: "Series", images: series.map((serie) => serie.image) },
-  { id: 3, name: "Cartoons", images: cartoons.map((cartoon) => cartoon.image) },
+  {
+    id: 1,
+    name: "Films",
+    images: films.map((film) => film.image),
+    filmId: films.map((film) => film.id),
+  },
+  {
+    id: 2,
+    name: "Series",
+    images: series.map((serie) => serie.image),
+    filmId: series.map((serie) => serie.id),
+  },
+  {
+    id: 3,
+    name: "Cartoons",
+    images: cartoons.map((cartoon) => cartoon.image),
+    filmId: cartoons.map((cartoon) => cartoon.id),
+  },
 ];
 
 const CategoryPage = () => {
@@ -23,11 +39,27 @@ const CategoryPage = () => {
 
   // Преобразуем id в число и находим соответствующую категорию
   const categoryId = parseInt(id as string, 10);
-  const category = categories.find((cat) => cat.id === categoryId);
+  const category = categories.find((cat) => cat.id === categoryId) || {
+    id: 0,
+    name: "Неизвестная категория",
+    images: [],
+    filmId: [],
+  };
 
   const [currentImage, setCurrentImage] = useState(0);
+  const [userAnswer, setUserAnswer] = useState("");
+  const [userScore, setUserScore] = useState(0);
 
-  const handleNext = () => {};
+  const handleNext = () => {
+    if (currentImage < category.images.length - 1 && userAnswer.length > 0) {
+      setCurrentImage((prevImage) => prevImage + 1);
+      console.log(category.filmId[currentImage - 1], currentImage);
+    }
+    setUserAnswer("");
+    // if ( === ) {
+
+    // }
+  };
 
   if (!category) {
     return <div>Категория не найдена</div>; // Если категория не найдена, выводим сообщение
@@ -39,12 +71,9 @@ const CategoryPage = () => {
 
       <div>
         <h2>Элементы категории:</h2>
-        <div>
-          {category.images.map((imageSrc, index) => (
-            // <Image key={index} src={imageSrc.src} alt={`Image ${index + 1}`} layout="fill" />
-            <GamePictures key={index} src={imageSrc.src} />
-          ))}
-        </div>
+        <div>{<GamePictures src={category.images[currentImage]} />}</div>
+        <input value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)} type="text" />
+        <button onClick={() => handleNext()}>КНОПОЧКА</button>
       </div>
     </div>
   );
